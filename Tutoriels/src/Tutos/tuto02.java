60e64e9e0e8b0d6dd3427814a8124880c523ce73
Regé Garcia Josias <garciagarcia293@gmail.com>
2023-07-31 00:17:48 +0200

    Commit 2

---
+++ Tutoriels/src/Tutos/tuto02.java
@@ -0,0 +1,56 @@
+package Tutos;
+
+public class tuto02 {
+	
+	public static void main(String[] args) {
+		
+		//The Float infinite and NaN numbers
+		 System.out.println("Positive Infinity for Float Numbers : " + Float.POSITIVE_INFINITY);
+		 System.out.println("Negative Infinity for Float Numbers : " + Float.NEGATIVE_INFINITY);
+		 System.out.println("Not a Number for Float Numbers : " + Float.NaN);
+		 
+		 System.out.println();
+		 
+		//The Double infinite and NaN numbers
+		 System.out.println("Positive Infinity for Double Numbers : " + Double.POSITIVE_INFINITY);
+		 System.out.println("Negative Infinity for Double Numbers : " + Double.NEGATIVE_INFINITY);
+		 System.out.println("Not a Number for Double Numbers : " + Double.NaN);
+		 
+		 System.out.println();
+		 
+		 //The Minimum and Maximum of Byte Numbers
+		 System.out.println("Mimimum of Byte Numbers : " + Byte.MIN_VALUE);
+		 System.out.println("Maximum of Byte Numbers : " + Byte.MAX_VALUE);
+		 
+		 System.out.println();
+		 
+		//The Minimum and Maximum of Short Numbers
+		 System.out.println("Mimimum of Short Numbers : " + Short.MIN_VALUE);
+		 System.out.println("Maximum of Short Numbers : " + Short.MAX_VALUE);
+		 
+		 System.out.println();
+		 
+		//The Minimum and Maximum of Integer Numbers
+		 System.out.println("Mimimum of Integer Numbers : " + Integer.MIN_VALUE);
+		 System.out.println("Maximum of Integer Numbers : " + Integer.MAX_VALUE);
+		 
+		 System.out.println();
+		 
+		//The Minimum and Maximum of Long Numbers
+		 System.out.println("Mimimum of Long Numbers : " + Long.MIN_VALUE);
+		 System.out.println("Maximum of Long Numbers : " + Long.MAX_VALUE);
+		 
+		 System.out.println();
+		 
+		//The Minimum and Maximum of Float Numbers
+		 System.out.println("Mimimum of Float Numbers : " + Float.MIN_VALUE);
+		 System.out.println("Maximum of Float Numbers : " + Float.MAX_VALUE);
+		 
+		 System.out.println();
+		 
+		//The Minimum and Maximum of of Double Numbers
+		 System.out.println("Mimimum of Double Numbers : " + Double.MIN_VALUE);
+		 System.out.println("Maximum of Double Numbers : " + Double.MAX_VALUE);
+
+	}
+}
